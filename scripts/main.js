bb364cd9b3f7df5797d6ec345f3060bd094b50d0
Byron Torres <btorres@thoughtworks.com>
2018-07-03 11:31:41 -0500

    Add button to add name in header.

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,32 @@ window.onload = function(){
   var myImage = document.querySelector('img');
 
   myImage.onclick = function() {
-      var mySrc = myImage.getAttribute('src');
-      if(mySrc === 'images/my-image.png') {
+  	var mySrc = myImage.getAttribute('src');
+  	if(mySrc === 'images/my-image.png') {
         myImage.setAttribute ('src','images/firefox-developer-logo.png');
-      } else {
-        myImage.setAttribute ('src','images/my-image.png');
-      }
+  	} else {
+  	  myImage.setAttribute ('src','images/my-image.png');
+  	}
+  }
+  // Personalized welcome message code
+
+  var myButton = document.querySelector('button');
+  var myHeading = document.querySelector('h1');
+
+  function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
   }
 }
